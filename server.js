abdb9e9e6a81694557568dfa642fcdf6fe7b9b36
shanghang <shangneu1992@gmail.com>
2016-08-01 19:46:32 -0400

    finish 5.17

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname+"/public"));
+app.listen(3001);
