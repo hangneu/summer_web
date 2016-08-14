e9732de4d6abcdf15a1cb14eaad1ef5b9aeb21fd
shanghang <shangneu1992@gmail.com>
2016-08-14 15:02:04 -0400

    register server side

--- assignment/services/user-service_server.js
+++ assignment/services/user-service_server.js
@@ -5,8 +5,18 @@ module.exports = function(app){
 		{_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
 		{_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
 		];
+
+	app.post("/api/user",createUser);
 	app.get("/api/user",getUsers);
 	app.get("/api/user/:userId",findUserById);
+
+
+	function createUser(req,res){
+		var user = req.body;
+		user._id = (new Date()).getTime()+"";
+		users.push(user);
+		res.send(user);
+	}
 
 	function getUsers(req,res){
 		var username = req.query['username'];
--- public/assignment/register_cotroller.js
+++ public/assignment/register_cotroller.js
@@ -3,7 +3,7 @@
 		.module("WebAppMaker")
 		.controller("RegisterController",RegisterController)
 
-		function RegisterController($location,UserService){
+		function RegisterController(UserService){
 			vm = this;
 			vm.register = register;
 			function register(username,password,password2){
